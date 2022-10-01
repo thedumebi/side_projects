cdd15ec4db3ba314f3a60b5ab4292fa1e9034385
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2022-10-01 09:08:46 +0100

    update hex fn and add rgb view

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -8,27 +8,35 @@ const hex = Array.from({ length: 15 }, (_, i) => {
 
 const getRandomNumber = () => Math.floor(Math.random() * hex.length);
 
+const randomHex = () => `#${Math.floor(Math.random() * 0xffffff).toString(16).padEnd(6, "0")}`;
+const hexToRgb = (hex) => {
+  hex = hex.replace(/^#?([0-9a-f]{6})$/i, "$1");
+  hex = Number(`0x${hex}`);
+  return [(hex >> 16) & 0xff, (hex >> 8) & 0xff, hex & 0xff];
+};
+
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+const colorRGB = document.querySelector('.color-rgb')
 
 btn.addEventListener("click", () => {
-  let hexColor = "#";
-  for (let i = 0; i < 6; i++) {
-    hexColor += hex[getRandomNumber()];
-  }
+  const hexColor = randomHex();
+  const rgbColor = `rgb(${hexToRgb(hexColor)})`
 
   document.querySelector(".container").style.backgroundColor = hexColor;
   color.textContent = hexColor;
   color.style.color = hexColor;
+  colorRGB.textContent = rgbColor;
+  colorRGB.style.color = rgbColor;
   btn.style.backgroundColor = hexColor;
 });
 
-let hexColor = "#";
-for (let i = 0; i < 6; i++) {
-  hexColor += hex[getRandomNumber()];
-}
+const hexColor = randomHex();
+const rgbColor = `rgb(${hexToRgb(hexColor)})`
 
 document.querySelector(".container").style.backgroundColor = hexColor;
 color.textContent = hexColor;
 color.style.color = hexColor;
+colorRGB.textContent = rgbColor;
+colorRGB.style.color = rgbColor;
 btn.style.backgroundColor = hexColor;
